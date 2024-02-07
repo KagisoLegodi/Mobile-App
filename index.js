020844e884a5d8e2b50f00bcb0b3cfe498250519
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-02-07 21:56:11 +0200

    appendItemToShoppingListEl(itemArray[i])

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-b6132-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -23,6 +23,16 @@ addButtonEl.addEventListener("click", function() {
     appendItemToShoppingListEl(inputValue)
     
     
+})
+
+
+onValue(shoppingListInDB, function (snapshot ){
+   let itemArray = Object.values(snapshot.val())
+   for (let i = 0; i < itemsArray.length; i++) {
+
+    appendItemToShoppingListEl(itemArray[i])
+     
+   }
 })
 
 function clearInputFieldEl() {
